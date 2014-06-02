0357cab6a83e89c97df8a84ef4686b8f56f87da6
Amy Rudolph <amy.rudolph@live.co.uk>
2014-06-02 18:06:56 +0400

    added devise gem

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,7 @@ Photoapp::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  # required for devise
+  config.action_mailer.default_url_options = {host: http://travelphotos.herokuapp.com/}
 end
