734d85ea7edf057a6d82e6771eda06433236a291
Amy Rudolph <amy.rudolph@live.co.uk>
2014-06-10 21:20:09 +0400

    ixed syntax error in production.rb missing '

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,5 +79,5 @@ Photoapp::Application.configure do
   config.log_formatter = ::Logger::Formatter.new
 
   # required for devise
-  config.action_mailer.default_url_options = {host: http://travelphotos.herokuapp.com/}
+  config.action_mailer.default_url_options = {host: 'http://travelphotos.herokuapp.com/'}
 end
